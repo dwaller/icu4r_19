39a040964057e3889ad3766e690aa764197db9d2
David Waller <david.waller@metaswitch.com>
2012-01-09 11:10:59 +0000

    Remove dependence on a very specific ICU version
    Used to depend on ICU 4.6, though the warning message talked about ICU 3.4.

--- extconf.rb
+++ extconf.rb
@@ -1,15 +1,11 @@
 require 'mkmf'
-$LDFLAGS = "-licuuc -licui18n -licudata -lstdc++ "
+$LDFLAGS = "-licuuc -licui18n -licudata -lstdc++"
 $CFLAGS = "-Wall"
-if !have_library('icui18n', 'u_init_46')
-  puts "ICU v3.4 required -- not found."
-  exit 1
-end
 create_makefile('icu4r')
 File.open("Makefile", "a") << <<-EOT
 
 check:	$(DLLIB)
-	@$(RUBY) $(srcdir)/test/test_ustring.rb 
+	@$(RUBY) $(srcdir)/test/test_ustring.rb
 	@$(RUBY) $(srcdir)/test/test_calendar.rb
 	@$(RUBY) $(srcdir)/test/test_converter.rb
 	@$(RUBY) $(srcdir)/test/test_collator.rb
